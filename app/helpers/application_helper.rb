3cb6a67e3b01102deb00c1b70b5672f06c9eb0fe
lucasilliano <lucas.illiano@hotmail.com>
2025-03-04 01:29:12 +0100

    adding markdown

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,16 @@
+# app/helpers/application_helper.rb
 module ApplicationHelper
+  def markdown(text)
+    renderer = Redcarpet::Render::HTML.new(
+      filter_html: true,   # Prevent raw HTML from being rendered
+      hard_wrap: true      # Add <br> for line breaks
+    )
+    options = {
+      autolink: true,
+      tables: true,
+      fenced_code_blocks: true,
+      lax_spacing: true
+    }
+    Redcarpet::Markdown.new(renderer, options).render(text).html_safe
+  end
 end
